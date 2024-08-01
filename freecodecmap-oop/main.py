7d7211fffcbc19d1be85185306252ef0cc743cbe
gokhan gokcen <gokhangokcenn@gmail.com>
2024-08-01 15:34:25 +0300

    freecodecamp oop

--- freecodecmap-oop/main.py
+++ freecodecmap-oop/main.py
@@ -1,47 +1,8 @@
-import csv
+from phone import Phone
+from keyboard import Keyboard
 
+item1 = Keyboard("jscKeyboard", 1000, 3)
 
-class Item:
-    pay_rate = 0.8 # The pay rate after %20 discount - attribute
-    all = []
-    def __init__(self, name: str, price: float, quantity=0):
-        # Run validations to the received arguments
-        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+item1.apply_discount()
 
-
-        # Assign to self objects
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        
-        # Actions to execute
-        Item.all.append(self)
-    
-    def calculate_total_price(self): # def in class called by "method"
-        return self.price * self.quantity
-    
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
-
-    @classmethod
-    def instantiate_from_csv(cls):
-        with open("items.csv", "r") as f:
-            reader = csv.DictReader(f)
-            items = list(reader)
-        
-        for item in items:
-            Item(
-                name=item.get('name'),
-                price=float(item.get('price')),
-                quantity=int(item.get('quantity')),
-            )
-
-    def __repr__(self):
-        return f"Item('{self.name}', {self.price}, {self.quantity})"
-
-
-Item.instantiate_from_csv()
-print(Item.all)
-
-# 1.03.45
+print(item1.price)
