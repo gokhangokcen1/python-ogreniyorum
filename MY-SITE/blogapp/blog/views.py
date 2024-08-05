f58dbb5c77d57484b0f82ea330d77b5cdef88a67
gokhan gokcen <gokhangokcenn@gmail.com>
2024-08-05 18:58:03 +0300

    dinamik veri ile calisma

--- MY-SITE/blogapp/blog/views.py
+++ MY-SITE/blogapp/blog/views.py
@@ -1,12 +1,47 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+data = {
+    "blogs": [
+        {
+            "id": 1,
+            "title": "komple web geliştirme",
+            "image": "1.jpg",
+            "is_active": True,
+            "is_home": False,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 2,
+            "title": "python kursu",
+            "image": "2.jpg",
+            "is_active": True,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 3,
+            "title": "django kursu",
+            "image": "3.jpg",
+            "is_active": False,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        }
+    ]
+}
+
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/index.html", context)
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/blogs.html", context)
 
 def blog_details(request, id):
     return render(request, "blog/blog-details.html", {
