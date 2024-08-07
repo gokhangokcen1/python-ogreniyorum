c344e112c780c6e3151e6e8d0297e264d48b5268
gokhan gokcen <gokhangokcenn@gmail.com>
2024-08-07 15:43:13 +0300

    image upload

--- MY-SITE/blogapp/blog/models.py
+++ MY-SITE/blogapp/blog/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    image = models.CharField(max_length=50)
+    image = models.ImageField(upload_to = "blogs")
     description = models.TextField()
     is_active = models.BooleanField()
     is_home = models.BooleanField()
