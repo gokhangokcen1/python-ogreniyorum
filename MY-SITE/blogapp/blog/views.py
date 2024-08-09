6784c2e3a5aeca4d7fc1feeeaa1ed85cf88c8d7b
gokhan gokcen <gokhangokcenn@gmail.com>
2024-08-09 11:13:52 +0300

    dinamik kategori menüsü

--- MY-SITE/blogapp/blog/views.py
+++ MY-SITE/blogapp/blog/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from blog.models import Blog
+from blog.models import Blog, Category
 
 data = {
     "blogs": [
@@ -34,7 +34,8 @@ data = {
 # Create your views here.
 def index(request):
     context = {
-        "blogs": Blog.objects.filter(is_home=True,is_active=True)
+        "blogs": Blog.objects.filter(is_home=True,is_active=True),
+        "categories": Category.objects.all()
     }
     return render(request, "blog/index.html", context)
 
@@ -49,3 +50,6 @@ def blog_details(request, slug):
     return render(request, "blog/blog-details.html", {
         "blog" : blog
     })
+
+def blogs_by_category(request, slug):
+    pass
