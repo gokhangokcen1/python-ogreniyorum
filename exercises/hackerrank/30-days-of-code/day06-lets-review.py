7ce75e867d558ab7cf1bc1db4f539dc7d99e05c4
gokhan gokcen <gokhangokcenn@gmail.com>
2024-08-09 21:58:59 +0300

    day05 loops and day06 lets review

---
+++ exercises/hackerrank/30-days-of-code/day06-lets-review.py
@@ -0,0 +1,24 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N = int(input("how many word\n"))
+
+
+result = []
+for i in range(N):
+    
+
+    S = input()   
+    word1 = ""
+    word2 = ""
+    for i in range(len(S)):
+        if i % 2 == 0:
+            word1 += S[i]
+        else:
+            word2 += S[i]
+
+    else:
+        result.append(word1+" "+word2)
+
+print(*result, sep="\n")
+
+
+
