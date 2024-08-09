e82c68ec00f256ec70469c9ec043a0d6ff5a7454
gokhan gokcen <gokhangokcenn@gmail.com>
2024-08-09 11:22:34 +0300

    many to one ilişki tipi

--- MY-SITE/blogapp/blog/models.py
+++ MY-SITE/blogapp/blog/models.py
@@ -4,24 +4,6 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to = "blogs")
-    description = RichTextField()
-    is_active = models.BooleanField()
-    is_home = models.BooleanField()
-    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
-
-    def __str__(self):
-        return f"{self.title}"
-    
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return f"{self.title}"
-
 class Category(models.Model):
     name = models.CharField(max_length=150)
     slug = models.SlugField(null=False,blank=True, unique=True, db_index=True, editable=False)
@@ -33,3 +15,26 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to = "blogs")
+    description = RichTextField()
+    is_active = models.BooleanField()
+    is_home = models.BooleanField()
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+    category = models.ForeignKey(Category,default=1, on_delete=models.CASCADE) 
+    #null=True, on_delete=SET_NULL bir kategori silindiğinde blog da silinmesin istersek böyle yapmamız gerekir.
+    # ya da default=1, on_delete=model.SET_DEFAULT default bir kategoriye ata
+    
+    def __str__(self):
+        return f"{self.title}"
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.title}"
+
