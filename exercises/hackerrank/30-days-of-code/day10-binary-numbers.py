15061fda5832590e664e5408c8445cdb24520b71
gokhan gokcen <gokhangokcenn@gmail.com>
2024-08-10 14:14:06 +0300

    day10-binary-numbers

---
+++ exercises/hackerrank/30-days-of-code/day10-binary-numbers.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    dec_to_bin = [*bin(n)[2:]]
+
+    left = 0
+    right = 0
+
+    for i in dec_to_bin:
+        if i != '0':
+            left +=1 
+        else:
+            break
+
+    for j in dec_to_bin[::-1]:
+        if j != '0':
+            right += 1
+        else:
+            break
+
+    print(max(left,right))
