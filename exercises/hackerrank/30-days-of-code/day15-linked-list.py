b5848d5a9cb2557d759095a77f7e170365051bf0
gokhan gokcen <gokhangokcenn@gmail.com>
2024-08-12 12:09:52 +0300

    day15-linked-list

--- exercises/hackerrank/30-days-of-code/day15-linked-list.py
+++ exercises/hackerrank/30-days-of-code/day15-linked-list.py
@@ -1,21 +1,21 @@
 class Node:
     def __init__(self,data):
-        self.data = data
+        self.data = data 
         self.next = None 
 class Solution: 
     def display(self,head):
-        current = head
+        current = head 
         while current:
-            print(current.data,end=' ')
-            current = current.next
+            print(current.data,end=' ') 
+            current = current.next  
 
     def insert(self,head,data): 
     #Complete this method
-        new_node = Node(data)
-        if head is None:
-            return new_node
+        new_node = Node(data)  
+        if head is None: 
+            return new_node 
         
-        last = head
+        last = head 
 
         while last.next:
             last = last.next
